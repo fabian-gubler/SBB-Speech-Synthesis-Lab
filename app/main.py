3b3f1d0ddf8e05e0bcf19f68c20ae5ec9c30fecc
Fabian Gubler <fabian.gubler@hotmail.com>
2023-04-24 23:00:06 +0200

    manual update

--- app/main.py
+++ app/main.py
@@ -13,22 +13,6 @@ import hashlib
 import xml.etree.ElementTree as ET
 import azure.cognitiveservices.speech as speechsdk
 
-# Loop over csv
-# - retrieve output text
-# - retrieve voice & slang from each row
-# - use <index>_<voice>_accent to set as output file name
-# - use function to generate sample
-
-# Advanced voice with SSML
-# link: https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup-voice
-# - retrieve effect, style, role
-# - retrieve speaking languages (accent)
-# - retrieve prosody (contour, pitch, rate, volume)
-# - adjust emphasis (strong, moderate, reduced)
-# - audio duration (speed)
-# - optional: background audio
-# - transform fields into xml
-
 
 # Read settings from JSON file
 with open("settings.json") as settings_file:
@@ -46,8 +30,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 
-def generate_ssml_text(text, voice, pitch, rate):
-
+def generate_ssml_text(text, pitch, rate, voice, role, style):
     prosody = {
         "pitch": pitch,  # low, medium, high
         "rate": rate,  # slow, medium, fast
@@ -55,27 +38,32 @@ def generate_ssml_text(text, voice, pitch, rate):
 
     voice_element = ET.Element("voice")
     voice_element.set("name", voice)
-    
-    prosody_element = ET.SubElement(voice_element, "prosody")
+
+    express_as_element = ET.SubElement(voice_element, "mstts:express-as")
+    express_as_element.set("role", role)  # Add role attribute
+    express_as_element.set("style", style)  # Add style attribute
+
+    prosody_element = ET.SubElement(express_as_element, "prosody")
     prosody_element.set("pitch", prosody["pitch"])
     prosody_element.set("rate", prosody["rate"])
     prosody_element.text = text
 
     root_element = ET.Element("speak")
     root_element.set("version", "1.0")
-    root_element.set("xml:lang", "de-DE")
+    root_element.set("xml:lang", "en_US") 
     root_element.set("xmlns:mstts", "http://www.w3.org/2001/mstts")
     root_element.append(voice_element)
 
     xml_string = ET.tostring(root_element, encoding="unicode")
     return xml_string
 
-def synthesize_speech(text, pitch, rate, voice, output_filename):
+def synthesize_speech(text, pitch, rate, voice, role, style, output_filename):
     speech_synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config, audio_config=None
     )
 
-    ssml_string = generate_ssml_text(text, voice, pitch, rate)
+    ssml_string = generate_ssml_text(text, pitch, rate, voice, role, style)
+    # print(ssml_string)
     result = speech_synthesizer.speak_ssml_async(ssml_string).get()
 
     stream = speechsdk.AudioDataStream(result)
@@ -97,11 +85,13 @@ with open("combinations.csv", "r", newline="", encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
 
     for index, row in enumerate(reader, start=1):
-        text = row["text"]
         voice = row["voice"]
+        text = row["text"]
         pitch = row["pitch"]
         rate = row["rate"]
+        role = row["role"]  # Read role option
+        style = row["style"]  # Read style option
 
-        output_filename = f"{index}_{voice}_{pitch}-pitch_{rate}-rate.wav"
+        output_filename = f"{index}_{voice}_style-{style}_role-{role}.wav"
         output_filepath = os.path.join(output_dir, output_filename)
-        synthesize_speech(text, voice, pitch, rate, output_filepath)
+        synthesize_speech(text, pitch, rate, voice, role, style, output_filename)

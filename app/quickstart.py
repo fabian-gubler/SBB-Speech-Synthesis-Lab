811f3baa46063ac677a8a98f2b907a116da7eb29
Fabian Gubler <fabian.gubler@hotmail.com>
2023-04-24 15:45:13 +0200

    manual update

--- app/quickstart.py
+++ app/quickstart.py
@@ -1,31 +1,19 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-
-# <code>
 import azure.cognitiveservices.speech as speechsdk
 
-# Creates an instance of a speech config with specified subscription key and service region.
-# Replace with your own subscription key and service region (e.g., "westus").
 speech_key, service_region = "98b6bdd8791d40889e42933be6ced7cd", "westeurope"
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-
-# Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
 speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
 
-# Creates a speech synthesizer using the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+output_filename = "output_audio.wav"
+audio_output_config = speechsdk.audio.AudioOutputConfig(filename=output_filename)
 
-# Receives a text from console input.
-print("Type some text that you want to speak...")
-text = input()
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output_config)
 
-# Synthesizes the received text to speech.
-# The synthesized speech is expected to be heard on the speaker with this line executed.
+text = "Type some text that you want to speak..."
 result = speech_synthesizer.speak_text_async(text).get()
 
-# Checks result.
 if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized to speaker for text [{}]".format(text))
+    print("Speech synthesized to [{}] for text [{}]".format(output_filename, text))
 elif result.reason == speechsdk.ResultReason.Canceled:
     cancellation_details = result.cancellation_details
     print("Speech synthesis canceled: {}".format(cancellation_details.reason))
@@ -33,5 +21,3 @@ elif result.reason == speechsdk.ResultReason.Canceled:
         if cancellation_details.error_details:
             print("Error details: {}".format(cancellation_details.error_details))
     print("Did you update the subscription info?")
-# </code>
-

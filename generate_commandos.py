8956b6037ccda45c10dce1699e8956e4cc043d10
Fabian Gubler <fabian.gubler@hotmail.com>
2023-05-16 04:59:26 +0200

    manual update

--- generate_commandos.py
+++ generate_commandos.py
@@ -17,7 +17,7 @@ def generate_random_part():
         'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo', 'f': 'foxtrot',
         'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett', 'k': 'kilo', 'l': 'lima',
         'm': 'mike', 'n': 'november', 'o': 'oscar', 'p': 'papa', 'q': 'quebec', 'r': 'romeo',
-        's': 'sierra', 't': 'tango', 'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray',
+        's': 'sierra', 't': 'tango', 'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'xray',
         'y': 'yankee', 'z': 'zulu'
     }
 
